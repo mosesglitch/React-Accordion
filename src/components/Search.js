61053758c20c3fc816cd539119bdf2d4324a490c
mosesglitch <mosesgachuru15@gmail.com>
2022-10-01 14:44:13 +0300

    map wiki searches

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 const Search = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+  console.log("i run on every render");
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+    };
+    // searchWiki();
+    // (async () => {
+    //   await axios.get("wiki.com");
+    // })();
+    // axios
+    //   .get("https://en.wikipedia.org/w/api.php", {
+    //     params: {
+    //       action: "query",
+    //       list: "search",
+    //       origin: "*",
+    //       format: "json",
+    //       srsearch: term,
+    //     },
+    //   })
+    //   .then((response) => {
+    //     console.log(response.data);
+    //   });
+  }, [term]);
 
   return (
     <div>
