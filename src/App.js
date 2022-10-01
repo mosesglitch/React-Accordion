70a06e0593383e11f7c083e0b12ff796a24d73e3
mosesglitch <mosesgachuru15@gmail.com>
2022-10-01 10:52:14 +0300

    add app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Accordion from "./components/Accordion";
 
-function App() {
+const tems = [
+  {
+    title: "What is react?",
+    content: "React is a component based js framework",
+  },
+  {
+    title: "why use react?",
+    content: "React uses modular components that do only one thing well",
+  },
+  { title: "How do you use react", content: "By creating components" },
+];
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordion />
     </div>
   );
-}
-
+};
 export default App;
