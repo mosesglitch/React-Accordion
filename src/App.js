fe54cc7c60579f10d72437aa472990719ebeabf7
mosesglitch <mosesgachuru15@gmail.com>
2022-10-02 13:02:28 +0300

    add manual navigation header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import Dropdown from "./components/Dropdown";
-// import Accordion from "./components/Accordion";
-// import Search from "./components/Search";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 import Translate from "./components/Translate";
+import Route from "./components/Route";
+import Header from "./components/Header";
 const items = [
   {
     title: "What is react?",
@@ -29,10 +31,27 @@ const options = [
   },
 ];
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
-      <Translate />
-      {console.log("why")}
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
